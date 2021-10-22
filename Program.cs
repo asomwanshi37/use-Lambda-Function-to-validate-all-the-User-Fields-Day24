df697543a9cce006ea68f25a0eb9df2c275d9836
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-10-23 03:44:02 +0530

    Valid Last Name

---
+++ Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace UserRegistrationUsingRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to the user registration problem");
+            Console.WriteLine("Enter the first name = ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter the last name = ");
+            string lastName = Console.ReadLine();
+            UserRegistrationRegex userRegistrationRegex = new UserRegistrationRegex();
+            Console.WriteLine(userRegistrationRegex.validateFirstName(firstName));
+            Console.WriteLine(userRegistrationRegex.validatelastName(lastName));
+        }
+    }
+}
