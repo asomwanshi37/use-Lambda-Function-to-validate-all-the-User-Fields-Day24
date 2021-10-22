6d0e301132c399074f993d3357c690be21d3e0f9
asomwanshi37 <asomwanshi37@gmail.co.com>
2021-10-23 04:05:01 +0530

    Write Junit Test to Validate the User Entry for FirstName,Lastname,Email,password and Mobile

---
+++ UserRegistration.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserRegistrationUsingRegex
+{
+    public class UserRegistration
+    {
+        private string message;
+        public UserRegistration(string message)
+        {
+            this.message = message;
+        }
+        public string checkForFirstName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForLastName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForEmail()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForMobile()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+        public string checkForPassword()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Entry is not successful";
+        }
+    }
+}
